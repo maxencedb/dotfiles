5d34aff596c8b6bd2c4deddd54e7a9c6839e4337
Maxence de Bergeyck <de-bro_m@epita.fr>
2015-07-13 16:20:43 +0200

    [DEV][TOOL] Added tools.sh script with general-purpose tools

---
+++ tools.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+#
+# This files contains general purpose tools for
+# subsequent scripts.
+
+# Exit program with a specified error message and output
+exit_on_error()
+{
+    local MSG=${1:-An unknown error occured}
+    local NUM=${2:-1}
+    1>&2 echo "Error: $MSG"
+    exit $NUM
+}
+
+# Test for a binary using type
+test_binary()
+{
+    type $1 >/dev/null 2>&1
+}
+
+
