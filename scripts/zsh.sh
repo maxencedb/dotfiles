f8799fc11413f0018c1c2b7af8acb5b8cb5c23a6
Maxence de Bergeyck <de-bro_m@epita.fr>
2015-07-13 18:01:16 +0200

    [DEV][ZSH] Added custom plugin to hold aliases and such

--- scripts/zsh.sh
+++ scripts/zsh.sh
@@ -12,4 +12,19 @@ done
 # Launch oh-my-zsh installer
 sh -c "$(wget https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
 
+# Add a custom plugin to zshrc for aliases
+cat << EOF
+# Variables
+export EDITOR="vim"
 
+# Functions
+function dush()
+{
+  SIZES=$(du -sh *) &&
+  echo $SIZES | grep -E '^[0-9.]+T' | sort -nr &&
+  echo $SIZES | grep -E '^[0-9.]+G' | sort -nr &&
+  echo $SIZES | grep -E '^[0-9.]+M' | sort -nr &&
+  echo $SIZES | grep -E '^[0-9.]+K' | sort -nr &&
+  echo $SIZES | grep -E '^[0-9.]+[[:space:]]' | sort -nr
+}
+EOF > $HOME/.oh-my-zsh/custom/safe.zsh
