1ee51ecfccc2727ecba7ddc45b4e64e747597fa9
Maxence de Bergeyck <de-bro_m@epita.fr>
2015-07-13 16:22:39 +0200

    [DEV][INST] Added install.sh script to launch all install scripts

---
+++ install.sh
@@ -0,0 +1,28 @@
+#!/bin/sh
+
+# Global variables
+ROOT_PATH=$(dirname "$(readlink -f "$0")")
+SCRIPT_PATH=$ROOT_PATH/scripts
+
+# Source shell tools
+source $ROOT_PATH/tools.sh
+
+# Test if script directory exists
+if [ ! -d "$SCRIPT_PATH" ]
+then
+    exit_on_error "script directory not found"
+fi
+
+echo "Launching all install scripts ..."
+
+# Loop on all shell scripts and source them
+for script in $(ls $SCRIPT_PATH | grep -E ".+\.sh$")
+do
+    echo "### $script"
+    . $SCRIPT_PATH/$script
+done
+
+echo "Done"
+
+exit 0
+
