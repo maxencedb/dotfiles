e18f0def545ca6d8787f1d01a3e382a467856218
Maxence de Bergeyck <maxence.debergeyck@gmail.com>
2021-02-26 09:23:36 +0100

    [DEV][GIT][I3][ZSH] functions, flameshot

--- files/safe.zsh
+++ files/safe.zsh
@@ -12,8 +12,33 @@ export PATH="${HOME}/bin:${PATH}"
 alias sl="ls"
 alias suod="sudo"
 alias poi="poweroff"
+alias dm="docker-compose"
+alias dr="docker"
+alias ku="kubectl"
+alias tmp='cd $(mktemp -d -p /tmp tmp.$(date +%H%M).XXXX)'
 
 # Functions
+function da()
+{
+    eval $(ssh-agent)
+    ssh-add ~/.ssh/ssl.key
+    ssh-add ~/.ssh/id_rsa
+}
+
+function arch()
+{
+    local NAME=$@
+    if [ -z "$NAME" ]
+    then
+        read "?Folder name ? : " NAME
+    fi
+    NAME=$(echo $NAME | tr ' ' _)
+
+    local FULL_NAME=$(date '+%Y-%m-%d')_${NAME:-$RANDOM}
+    mkdir -p ~/archives/$FULL_NAME
+    cd ~/archives/$FULL_NAME
+}
+
 function ms()
 {
     mpv --no-video --ytdl-format=bestaudio ytdl://ytsearch:"$*"
